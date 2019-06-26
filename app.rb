17f466a8f3d8c021f9da9e0297c6ff15b9855f24
Jason Leach <jlspursfan@gmail.com>
2019-06-26 04:22:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,25 +3,23 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-  get '/pirates' do
-    @pirates = Pirate.all
-    erb :'pirates/index'
-  end
+    get '/pirates' do
+      @pirates = Pirate.all
+      erb :'pirates/index'
+    end
 
-  get '/pirates/new' do
-    erb :'pirates/new'
-  end
+    get '/pirates/new' do
+      erb :'pirates/new'
+    end
 
-  get '/pirates/:id' do
-    @pirate = Pirate.find(params[:id])
-    erb :'pirates/show'
-  end
+    get '/pirates/:id' do
+      @pirate = Pirate.find(params[:id])
+      erb :'pirates/show'
+    end
 
-  post '/pirates' do
-    pirate = Pirate.new(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:height])
-  end
-
-
+    post '/pirates' do
+      pirate = Pirate.new(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:height])
+    end
 
   end
 end
