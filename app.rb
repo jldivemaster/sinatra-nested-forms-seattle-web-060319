58330365c6494f6985387a7baead03c15ef3aa23
Jason Leach <jlspursfan@gmail.com>
2019-06-26 04:29:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      erb :'/root'
+    end
+
     get '/pirates' do
       @pirates = Pirate.all
       erb :'pirates/index'
